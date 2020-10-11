f12c83310094dda351795846e39e19c3d3d53238
Ram Pukar <ram_mosone@yahoo.com>
2020-10-11 09:14:46 +0545

    Add New extra page.

--- index.php
+++ index.php
@@ -3,16 +3,18 @@
         <!-- Content Wrapper. Contains page content -->
         <div class="content-wrapper">
             <?php 
-            // include('pages/home.php'); 
+            include('pages/home.php'); 
             // include('pages/category.php'); 
             // include('pages/category_list.php'); 
             // include('pages/404.php'); 
-            include('pages/500.php'); 
+            // include('pages/500.php'); 
             ?>
         </div>
         <!-- /.content-wrapper -->
     <?php include('includes/footer.php'); ?>
 
-    <?php //include('pages/recover_passwor.php'); ?>
+     <?php //include('pages/recover_passwor.php'); ?>
     <?php //include('pages/forgot_password.php'); ?>
-
+    <?php //include('pages/login.php'); ?>
+    <?php //include('pages/register.php'); ?>
+    <?php //include('pages/lockscreen.php'); ?>
